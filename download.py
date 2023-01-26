9f4fe8f8ad05950a143afcfdb0ea4ae1079a17aa
Ian Main <ian@pandadatasystems.com>
2023-01-26 15:40:42 -0800

    Update download.py

--- download.py
+++ download.py
@@ -1,13 +1,19 @@
 import pafy
 import re
 import wget
+import string
+import random
+import os
 
-url = "https://youtu.be/S2k99-bopIc"
+url = "https://youtu.be/8EpxlXzkRy0"
 
 exp = "^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*"
 s = re.findall(exp,url)[0][-1]
+letters = string.ascii_uppercase
 thumbnail = f"https://i.ytimg.com/vi/{s}/maxresdefault.jpg"
+file = f"{s}-{( ''.join(random.choice(letters) for i in range(10)) )}.jpg"
 wget.download(thumbnail)
+os.rename("maxresdefault.jpg", file)
 print(thumbnail)
 
 result = pafy.new(url)
@@ -21,3 +27,4 @@ best_quality_video = result.getbestvideo(preftype="mp4")
 print(best_quality_video)
 
 best_quality_video.download()
+
